ce15deefb7fd5cecd4190ca8aafafde109dfbeb4
Minecraft Modder <you@example.com>
2026-07-16 08:24:32 -0400

    Add craftable Burger item
    Vertical column recipe: bread, cooked porkchop, bread crafts into an
    edible Burger. Adds a custom creative tab since vanilla's item group
    registry keys are private and can't be referenced directly.

--- src/main/java/com/example/mob/CustomMobMod.java
+++ src/main/java/com/example/mob/CustomMobMod.java
@@ -1,17 +1,37 @@
 package com.example.mob;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class CustomMobMod implements ModInitializer {
 
 	public static EntityType<CustomCreeperEntity> CUSTOM_CREEPER;
+
+	public static final Item BURGER = new Item(new Item.Settings().food(
+		new FoodComponent.Builder()
+			.hunger(8)
+			.saturationModifier(0.8f)
+			.build()
+	));
+
+	public static final RegistryKey<ItemGroup> CUSTOM_GROUP = RegistryKey.of(
+		RegistryKeys.ITEM_GROUP,
+		new Identifier("custommob", "custom_group")
+	);
 
 	@Override
 	public void onInitialize() {
@@ -24,6 +44,18 @@ public class CustomMobMod implements ModInitializer {
 		);
 
 		FabricDefaultAttributeRegistry.register(CUSTOM_CREEPER, CreeperEntity.createCreeperAttributes());
+
+		Registry.register(Registries.ITEM, new Identifier("custommob", "burger"), BURGER);
+
+		Registry.register(
+			Registries.ITEM_GROUP,
+			CUSTOM_GROUP,
+			FabricItemGroup.builder()
+				.displayName(Text.translatable("itemGroup.custommob.custom_group"))
+				.icon(() -> new ItemStack(BURGER))
+				.entries((context, entries) -> entries.add(BURGER))
+				.build()
+		);
 
 		System.out.println("Custom Mob Mod initialized!");
 	}
